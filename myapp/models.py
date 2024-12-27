b0f25a95475018487945e2e5370fe479f7e2e6aa
R Dwaraknath <78579652+rdwarakreddy@users.noreply.github.com>
2024-12-27 16:40:53 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -7,13 +7,13 @@ from django.conf import settings
 class MovieReview(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     movie_name = models.CharField(max_length=100)
-    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],help_text="Rating should be between 1 and 5")
+    rating = models.FloatField(validators=[MinValueValidator(1),MaxValueValidator(5)],help_text="Rating should be between 1 and 5")
     review = models.TextField()
     photo = models.ImageField(upload_to='movie_photos/',null = True,blank = True)
     link = models.URLField(max_length=500,null = True,blank = True)
 
     def __str__(self):
-        return f"{self.movie_name} - {self.user.username}"
+        return f"{self.movie_name} - {self.rating}"
 
 class Like(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE)
